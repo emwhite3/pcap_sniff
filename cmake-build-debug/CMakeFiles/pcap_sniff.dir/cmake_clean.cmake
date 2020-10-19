084bdc81a1a192c92333b15c47ea7f51a15955cb
emwhite3 <emwhite3@miners.utep.edu>
2020-10-19 12:55:31 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/pcap_sniff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcap_sniff.dir/library.c.o"
+  "libpcap_sniff.a"
+  "libpcap_sniff.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pcap_sniff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef PCAP_SNIFF_LIBRARY_H
+#define PCAP_SNIFF_LIBRARY_H
+
+void hello(void);
+
+#endif //PCAP_SNIFF_LIBRARY_H
